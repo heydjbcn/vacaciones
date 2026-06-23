500427d300afc6b76d374f9598319ad00db89bc8
Mauri <j.maurim@gmail.com>
2026-06-23 21:20:05 +0200

    Clavar diseno: 4 temas (Cuaderno/Sereno/Papel/Nocturno), celdas exactas, leyenda, boton dinamico

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Service worker: app-shell offline. Los datos (/api/state) van siempre por red.
-const CACHE = 'vacaciones-v3';
+const CACHE = 'vacaciones-v4';
 const SHELL = [
   '/',
   '/index.html',
