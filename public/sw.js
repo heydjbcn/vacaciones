db4c7193efabdc6d6affe16a8ce2420b45a730e5
Mauri <j.maurim@gmail.com>
2026-06-23 21:26:39 +0200

    Solo tema Cuaderno (sin tabs) + contenido mas ancho (1280)

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Service worker: app-shell offline. Los datos (/api/state) van siempre por red.
-const CACHE = 'vacaciones-v6';
+const CACHE = 'vacaciones-v7';
 const SHELL = [
   '/',
   '/index.html',
